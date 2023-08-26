56040e0366d33058e7e94d0a996305f14dc8ec05
TuanDe2k3 <tuancoder2k3@gmail.com>
2023-08-26 09:57:38 +0700

    load project

---
+++ main.js
@@ -0,0 +1,22 @@
+
+// Gioi thieu (Select Image)
+document.querySelectorAll('#gallery + div > img').forEach(item =>{
+    item.addEventListener('click', function(){
+        document.getElementById('gallery').src = this.src
+    })
+})
+
+// Menu Navbar
+let menu = document.getElementById('menu')
+menu.addEventListener('click', ()=>{
+    if (menu.classList.contains('bx-menu')){
+        menu.classList.add('bx-x')
+        menu.classList.remove('bx-menu')
+    }
+    else{
+        menu.classList.remove('bx-x')
+        menu.classList.add('bx-menu')
+    }
+       
+    document.querySelector('nav').classList.toggle('open')
+})
